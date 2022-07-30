b38e61b1d7f2908dcf816317ed8fffb5f6258447
raphael <raphaelrcbarbosa@gmail.com>
2022-07-30 17:25:25 -0300

    Created Menu to select operations

--- desafio-core.py
+++ desafio-core.py
@@ -1,1 +1,32 @@
-print("Hello World")
+def menu():
+
+    menu = {}
+    menu['1']="Check postgres connection." 
+    menu['2']="Add entry to database."
+    menu['3']="See entrie in database"
+    menu['4']="Convert to XML." 
+    menu['5']="Exit"
+    while True: 
+        options=menu.keys()
+        for entry in options: 
+            print (entry, menu[entry])
+        
+        selection=input("Please Select:") 
+        if selection =='1': 
+            print( "1 selected") 
+        elif selection == '2': 
+            print( "2 selected")
+        elif selection == '3':
+            print( "3 selected") 
+        elif selection == '4':
+            print("4 selected")
+        elif selection == '5':     
+            break
+        else: 
+            print( "Unknown Option Selected!") 
+
+    
+
+
+if __name__ == "__main__":
+    menu()
